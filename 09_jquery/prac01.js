feaae370a655a2410ae3680d32d9a679658afada
MinSeon <s_2___@naver.com>
2022-10-12 12:29:05 +0900

    web study: jQuery

---
+++ 09_jquery/prac01.js
@@ -0,0 +1,15 @@
+$(".apple").click(function () {
+  $(".img").attr("src", "./image/apple.jpg");
+});
+
+$(".banana").click(function () {
+  $(".img").attr("src", "./image/bananas.jpg");
+});
+
+$(".grape").click(function () {
+  $(".img").attr("src", "./image/grapes.jpg");
+});
+
+$(".peach").click(function () {
+  $(".img").attr("src", "./image/peaches.jpg");
+});
